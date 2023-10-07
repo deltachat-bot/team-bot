1f0f520c9e50f9e6fe9af726e6496f7695339b1b
missytake <missytake@systemli.org>
2023-10-07 15:57:11 +0200

    test: check whether a chat is a relay group

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -1,4 +1,4 @@
-from teams_bot.bot import get_crew_id
+from teams_bot.bot import get_crew_id, RelayPlugin
 
 
 def test_get_crew_id(crew):
@@ -42,3 +42,36 @@ def test_disable_old_crew(crew, outsider):
     quit_message = crew.user.wait_next_incoming_message()
     assert "There is a new Group for the Team now" in quit_message.text
     assert outsider.get_config("addr") in quit_message.text
+
+
+def test_is_relay_group(crew, outsider):
+    crew.bot.relayplugin = RelayPlugin(crew.bot)
+    assert not crew.bot.relayplugin.is_relay_group(crew)
+
+    botcontact_outsider = outsider.create_contact(crew.bot.get_config("addr"))
+    outsider_to_bot = outsider.create_chat(botcontact_outsider)
+    outsider_to_bot.send_text("test message to bot")
+    message_from_outsider = crew.bot.wait_next_incoming_message()
+    assert not crew.bot.relayplugin.is_relay_group(message_from_outsider.chat)
+
+    outsider_to_bot = outsider.create_group_chat(
+        "test with outsider", contacts=[botcontact_outsider]
+    )
+    outsider_to_bot.send_text("test message to outsider group")
+    message_from_outsider = crew.bot.wait_next_incoming_message()
+    assert not crew.bot.relayplugin.is_relay_group(message_from_outsider.chat)
+
+    botcontact_user = crew.user.create_contact(crew.bot.get_config("addr"))
+    user_to_bot = crew.user.create_chat(botcontact_user)
+    user_to_bot.send_text("test message to bot")
+    message_from_user = crew.bot.wait_next_incoming_message()
+    assert not crew.bot.relayplugin.is_relay_group(message_from_user.chat)
+
+    user_group = crew.user.create_group_chat(
+        "test with user", contacts=[botcontact_user]
+    )
+    user_group.send_text("testing message to user group")
+    message_from_user = crew.bot.wait_next_incoming_message()
+    assert not crew.bot.relayplugin.is_relay_group(message_from_user.chat)
+
+    # get example from actual relay group as soon as it works

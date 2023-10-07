defb4c10fd5fa163b2e7522a7c226e68e1bf3b38
missytake <missytake@systemli.org>
2023-10-07 10:09:55 +0200

    added first basic CLI test

---
+++ tests/test_cli.py
@@ -0,0 +1,10 @@
+def test_help(cmd):
+    cmd.run_ok(
+        [],
+        """
+        Usage: teams-bot [OPTIONS] COMMAND [ARGS]...
+        * -h, --help  Show this message and exit.
+        * init  Configure bot; create crew; add user to crew by scanning a QR code.
+        """
+    )
+

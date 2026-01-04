c86fe3b0dd4c1a977f9c5380780c8729050c5bdd
missytake <missytake@systemli.org>
2026-01-04 14:20:15 +0100

    handle system messages and commands in crew chat

--- src/team_bot/relay.py
+++ src/team_bot/relay.py
@@ -3,7 +3,8 @@ import json
 
 from deltachat_rpc_client import events, EventType
 
-from .util import get_crew_id
+from .util import get_crew_id, get_relay_group, reply
+from .commands import crew_help, set_display_name, set_avatar, start_chat, set_outside_help
 
 log = logging.getLogger("root")
 relayhooks = events.HookCollection()
@@ -30,7 +31,62 @@ def incoming_message(event):
     log.debug(event)
     msg = event.message_snapshot
     account = msg.chat.account
+    event.account = account
     crew_id = get_crew_id(event)
 
     import pdb; pdb.set_trace()
-    
+    if msg.is_info:
+        if msg.chat_id == crew_id:
+            log.debug("Ignoring system message in crew.")
+            return
+        if not get_relay_group(msg.chat):
+            log.debug(f"Ignoring system message in the relay group {msg.chat_id}")
+        else:
+            log.debug(f"This is a system message in the outside chat {msg.chat_id}")
+            relay_group = get_relay_group(msg.chat)
+            if "image changed by" in msg.text:
+                relay_group.set_image(msg.chat.get_full_snapshot().profile_image)
+            if "name changed from" in msg.text:
+                group_name = "[%s] %s" % (
+                    account.get_config("addr").split("@")[0],
+                    msg.chat.get_full_snapshot().name,
+                )
+                relay_group.set_name(group_name)
+        return
+
+    if msg.chat_id == crew_id:
+        if msg.text.startswith("/"):
+            log.debug(f"handling command by {msg.sender.get_snapshot().name_and_addr}: {msg.text}")
+            arguments = msg.text.split(" ")
+            if arguments[0] == "/help":
+                reply(msg.chat, crew_help(), quote=msg.message)
+            if arguments[0] == "/set_name":
+                displayname = msg.text.split("/set_name ")[1]
+                reply(
+                    msg.chat,
+                    set_display_name(account, displayname),
+                    quote=msg.message,
+                )
+            if arguments[0] == "/set_avatar":
+                result = set_avatar(account, msg, msg.chat)
+                reply(msg.chat, result, quote=msg.message)
+            if arguments[0] == "/generate_invite":
+                reply(msg.chat, account.get_qr_code(), quote=msg.message)
+            if arguments[0] == "/start_chat":
+                outside_chat, result = start_chat(account, msg)
+                if "success" in result:
+                    for msg in outside_chat.get_messages():
+                        pass  # :todo self.forward_to_relay_group(msg, started_by_crew=True)
+                reply(msg.chat, result, quote=msg.message)
+            if arguments[0] == "/set_outside_help":
+                try:
+                    help_message = msg.text.split("/set_outside_help ")[1]
+                except IndexError:
+                    set_outside_help(account, "")
+                    return reply(msg.chat,"Removed help message for outsiders", quote=msg.message)
+                set_outside_help(account, help_message)
+                reply(msg.chat,f"Set help message for outsiders to {help_message}", quote=msg.message)
+        else:
+            logging.debug("Ignoring message, just the crew chatting")
+
+    # see RelayPlugin.ac_incoming_message()
--- src/team_bot/util.py
+++ src/team_bot/util.py
@@ -1,6 +1,7 @@
+import json
 from typing import Optional
 
-from deltachat_rpc_client import Account
+from deltachat_rpc_client import Account, Chat, Message
 from deltachat_rpc_client._utils import AttrDict
 
 
@@ -18,3 +19,25 @@ def get_crew_invite(account: Account) -> str:
         crew_invite = account.get_qr_code()
         account.crew_invite = crew_invite
     return crew_invite
+
+
+def get_relay_groups(account: Account) -> [(int, int)]:
+    """Get a list of all relay groups"""
+    relay_json = account.get_config("ui.relay_groups")
+    return json.loads(relay_json)
+
+
+def get_relay_group(outside_chat: Chat) -> Chat:
+    """Return Relay group for an outside chat, return None if there is none."""
+    for mapping in get_relay_groups(outside_chat.account):
+        if mapping[0] == outside_chat.id:
+            return outside_chat.account.get_chat_by_id(mapping[1])
+
+
+def reply(chat: Chat, text: str, attachment: str = None, quote: Message = None):
+    """Reply to a chat, with a text, optionally including an attachment or quoting a message."""
+    chat.send_message(
+        text=text,
+        file=attachment,
+        quoted_msg=quote,
+    )

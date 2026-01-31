49f57362ef3516ccc5dd17aa60f226c5ab92b019
missytake <missytake@systemli.org>
2026-01-31 01:37:03 +0100

    tests: get more tests running
    upgrade deltachat-rpc-client to 2.38.0

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,39 +2,23 @@ import logging
 import os
 
 import pytest
-from deltachat_rpc_client._utils import AttrDict
 from deltachat_rpc_client.events import EventType
 
 from team_bot.relay import relayhooks
 from team_bot.setup import setuphooks
 
+INDEFINITELY = lambda _: False
+
 
 @pytest.fixture
-def bot(acfactory, log):
+def bot(acfactory, log, caplog):
     assert os.getenv("CHATMAIL_DOMAIN")
     log.step("Configuring Bot")
     bot = acfactory.new_configured_bot()
     bot.account.set_config("displayname", "Bot from TEST team")
     bot.add_hooks(setuphooks)
-    bot.logger = logging.getLogger("root")
-
-    def process_events(bot, until: EventType) -> AttrDict:
-        while True:
-            event = bot.account.wait_for_event()
-            if event.get("msg"):
-                print(event.get("msg"))
-            event["kind"] = EventType(event.kind)
-            event["account"] = bot.account
-            bot._on_event(event)
-            if event.kind == EventType.INCOMING_MSG:
-                bot._process_messages()
-                return bot.account.get_message_by_id(event.msg_id).get_snapshot()
-            if until == event.kind:
-                return event
-
-    bot.process_events = process_events
     bot.account.start_io()
-    bot.process_events(bot, EventType.IMAP_INBOX_IDLE)
+    bot._process_events(INDEFINITELY, until_event=EventType.IMAP_INBOX_IDLE)
     return bot
 
 
@@ -53,13 +37,14 @@ def crew(crew_member, bot, log, caplog):
     bot_invite = bot.account.get_qr_code()
     crew_member.secure_join(bot_invite)
 
-    bot.process_events(bot, EventType.SECUREJOIN_INVITER_PROGRESS)
+    bot._process_events(INDEFINITELY, until_event=EventType.SECUREJOIN_INVITER_PROGRESS)
 
     log.step("Bot changes hooks")
     for hook, event in setuphooks:
         bot.remove_hook(hook, event)
     bot.add_hooks(relayhooks)
 
+    crew_member.wait_for_incoming_msg()
     for chat in crew_member.get_chatlist(snapshot=True):
         if chat.chat_type == "Group":
             return chat

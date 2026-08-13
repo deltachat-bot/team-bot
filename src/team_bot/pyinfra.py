0e3df9d22c1e7a640155d0c3073600eba3189dba
missytake <missytake@systemli.org>
2026-08-13 13:21:29 +0200

    fix: add imports

--- src/team_bot/pyinfra.py
+++ src/team_bot/pyinfra.py
@@ -1,6 +1,8 @@
 import importlib.resources
 from io import StringIO
 
+from pyinfra import host
+from pyinfra.facts.files import File
 from pyinfra.operations import files, git, server, systemd
 
 
@@ -96,7 +98,7 @@ def deploy_team_bot(
         _use_su_login=True,
     )
 
-    if not host.get_fact(files.File, f"/var/lib/systemd/linger/{unix_user}"):
+    if not host.get_fact(File, f"/var/lib/systemd/linger/{unix_user}"):
         server.shell(
             name=f"enable {unix_user}'s systemd units to auto-start at boot",
             commands=[f"loginctl enable-linger {unix_user}"],

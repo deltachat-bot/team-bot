40ee6e1a4c69ca217131d4dd246fffb4ee3431db
missytake <missytake@systemli.org>
2026-09-19 22:21:52 +0200

    fix: accept messages from mailing lists

--- src/team_bot/forwarding.py
+++ src/team_bot/forwarding.py
@@ -78,6 +78,8 @@ def forward_to_relay_group(msg: AttrDict, started_by_crew: bool = False):
             outside_members.remove(account.self_contact)
             recipients = ", ".join([member.get_snapshot().display_name for member in outside_members])
             recipients = " and ".join(recipients.rsplit(", ", 1))
+        if outside_chat.get_basic_snapshot().chat_type == "Mailinglist":
+            recipients = msg.sender.get_snapshot().name_and_addr
         else:
             recipients = outside_chat.get_contacts()[0].get_snapshot().name_and_addr
         if started_by_crew:
